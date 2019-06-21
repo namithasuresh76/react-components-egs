93840c5d9a8af893ac0eae64cd7c22a547798762
namitha <namithasuresh76@gmail.com>
2019-06-21 11:52:35 +0530

    second commit

--- App.js
+++ App.js
@@ -25,24 +25,81 @@
 
 // export default App;
 
+// import React from 'react';
+// import './App.css';
+// import Courses from "./Courses";
+
+// let courses=[
+//     {id:1,name:"React", price:20000, duration:20},
+//     {id:2,name:"Angular", price:230000, duration:20},
+//     {id:3,name:"Java", price:20000, duration:20},
+//     {id:4,name:"JavaScript", price:230000, duration:20},
+   
+// ]
+
+// function app(){
+//     return(
+//       <div>
+//         <Courses data={courses}/>
+//         <Courses data={[]}/>
+//       </div>
+//     );
+// }
+// export default app
+
+// import React from 'react';
+// import './App.css';
+// import Courses from './Courses';
+
+// const courses=[
+//   'React',
+//   'Angular',
+//   'Java 8',
+//   'Spring',
+//   'Hibernate'
+// ]
+
+// function App() {
+//   return (
+//       <div>
+//          <Courses data={courses}/>
+//       </div>
+//   );
+// }
+
+// export default App;
+
+
+
+
 import React from 'react';
 import './App.css';
-import Courses from "./Courses";
+import Courses from './Courses';
 
-let courses=[
-    {id:1,name:"React", price:20000, duration:20},
-    {id:2,name:"Angular", price:230000, duration:20},
-    {id:3,name:"Java", price:20000, duration:20},
-    {id:4,name:"JavaScript", price:230000, duration:20},
-   
-]
+class App extends React.Component {
 
-function app(){
-    return(
-      <div>
-        <Courses data={courses}/>
-        <Courses data={[]}/>
-      </div>
+  constructor(props){ 
+    super(props)
+    this.courses=[
+      'React',
+      'Angular',
+      'Java 8',
+      'Spring',
+      'Hibernate'
+    ];
+    this.handleCourseClick = this.handleCourseClick.bind(this);
+  }
+  
+  handleCourseClick(event){
+    console.log('came inside the parent method of handle click ', event.target.id);
+  }
+  render(){ 
+    return (
+        <div>
+          <Courses data={this.courses} handleClick={this.handleCourseClick}/>
+        </div>
     );
+  }
 }
-export default app
+
+export default App;
--- Courses.js
+++ Courses.js
@@ -17,28 +17,66 @@
 
 
 
-import React from 'react';
-class Courses extends React.Component{
-    constructor(props){
-        super(props)
-    }
-    render(){
-        console.log(this.props)
-        return(
-            <div>
-                {this.props.data.length>0 ? (
-                <ul>
-                    {
-                        this.props.data.map(course=><li key={course.name}> Course Name:{course.name}  <br/>
-                            Course price:{course.price}  <br/>
-                            Course Duration:{course.duration}</li>)
-                    }
-                    </ul>
-                    ): <h1> there are no elements</h1>}
+// import React from 'react';
+// class Courses extends React.Component{
+//     constructor(props){
+//         super(props)
+//     }
+// handle()
+// {
+//     console.log(event.target.id);
+// }
+
+//     render(){
+//         console.log(this.props)
+//         return(
+//             <div>
+//                 {this.props.data.length>0 ? (
+//                 <ul>
+//                     {
+//                         this.props.data.map(course=><li key={course.name}> Course Name:{course.name}  <br/>
+//                             Course price:{course.price}  <br/>
+//                             Course Duration:{course.duration}</li>)
+//                     }
+//                     </ul>
+//                     ): <h1> there are no elements</h1>}
         
-                    </div>
-                    )}
+//                     </div>
+//                     )}
                     
         
+//     }
+// export default Courses;
+
+
+import React from 'react';
+
+class Courses extends React.Component {
+   
+    constructor(props){
+        super(props);
+        //this.handleCourseClick = this.handleCourseClick.bind(this);
     }
+
+
+    render(){
+        console.log(this.props);
+        return (
+            <div>
+                {
+                    this.props.data.length > 0 ? (
+                        <ul>
+                        {
+                            this.props.data.map(course => <li id={course}
+                                    key={course} 
+                                    onClick={(event)=> this.props.handleClick(event)}>{course}</li>)
+                        }
+                        </ul>
+                    ): <h1> There are no Courses for Display </h1>
+                }
+            </div>  
+        );
+    }
+}
+
 export default Courses;
